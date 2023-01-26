2cb041a1f69bf671acba9fb94326b401cd804121
Naimul Islam <ms0413354@gmail.com>
2023-01-26 12:40:50 +0600

    Add Form

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SingUp from './Components/App/SingUp';
+
+const App = () => {
+	return (
+		<div className="container">
+			<SingUp />
+		</div>
+	);
+};
+
+export default App;
