c253e1cef27fe2f529b7014ef5e6737e311f4732
Naimul Islam <ms0413354@gmail.com>
2023-01-26 12:27:27 +0600

    Formik validation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React from 'react';
+import FileUpload from 'react-mui-fileuploader';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default function App() {
+	const handleFileUploadError = (error) => {
+		// Do something...
+	};
 
-export default App;
+	const handleFilesChange = (files) => {
+		// Do something...
+		console.log(files);
+	};
+
+	return (
+		<FileUpload
+			getBase64={false}
+			multiFile={true}
+			disabled={false}
+			title="My awesome file uploader"
+			header="[Drag to drop]"
+			leftLabel="or"
+			rightLabel="to select files"
+			buttonLabel="click here"
+			buttonRemoveLabel="Remove all"
+			maxFileSize={0}
+			maxUploadFiles={0}
+			maxFilesContainerHeight={357}
+			acceptedType={'image/*'}
+			errorSizeMessage={'fill it or remove it to use the default error message'}
+			//allowedExtensions={["jpg", "jpeg"]}
+			onFilesChange={handleFilesChange}
+			onError={handleFileUploadError}
+			//imageSrc={'path/to/custom/image'}
+			BannerProps={{ elevation: 0, variant: 'outlined' }}
+			onContextReady={(context) => {}}
+			PlaceholderGridProps={{ md: 6 }}
+			LabelsGridProps={{ md: 6 }}
+			ContainerProps={{
+				elevation: 0,
+				variant: 'outlined',
+				sx: { p: 1 },
+			}}
+			placeholderImageDimension={{
+				xs: { width: 128, height: 128 },
+				sm: { width: 128, height: 128 },
+				md: { width: 164, height: 164 },
+				lg: { width: 256, height: 256 },
+			}}
+		/>
+	);
+}
