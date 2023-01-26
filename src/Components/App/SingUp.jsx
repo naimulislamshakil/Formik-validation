1d2e3944761dd96ce7634417c8271e212a670937
Naimul Islam <ms0413354@gmail.com>
2023-01-26 13:04:46 +0600

    Get data

--- src/Components/App/SingUp.jsx
+++ src/Components/App/SingUp.jsx
@@ -1,6 +1,23 @@
+import { useFormik } from 'formik';
 import React from 'react';
 
+const initialValues = {
+	name: '',
+	email: '',
+	password: '',
+	retype_password: '',
+};
+
 const SingUp = () => {
+	const { values, touched, handleBlur, handleChange, handleSubmit } = useFormik(
+		{
+			initialValues: initialValues,
+			onSubmit: (values) => {
+				console.log(values);
+			},
+		}
+	);
+
 	return (
 		<>
 			<section className="vh-50" style={{ backgroundColor: '#eee' }}>
@@ -15,86 +32,101 @@ const SingUp = () => {
 												Sign up
 											</p>
 
-											<form className="mx-1 mx-md-4">
+											<form onSubmit={handleSubmit} className="mx-1 mx-md-4">
 												<div className="d-flex flex-row align-items-center mb-4">
 													<i className="fas fa-user fa-lg me-3 fa-fw"></i>
 													<div className="form-outline flex-fill mb-0">
+														<label
+															htmlFor="name"
+															className="form-label"
+															for="form3Example1c"
+														>
+															Your Name
+														</label>
 														<input
 															type="text"
 															id="form3Example1c"
 															className="form-control"
+															name="name"
+															value={values.name}
+															onChange={handleChange}
+															onBlur={handleBlur}
 														/>
-														<label className="form-label" for="form3Example1c">
-															Your Name
-														</label>
 													</div>
 												</div>
 
 												<div className="d-flex flex-row align-items-center mb-4">
 													<i className="fas fa-envelope fa-lg me-3 fa-fw"></i>
 													<div className="form-outline flex-fill mb-0">
-														<input
-															type="email"
-															id="form3Example3c"
-															className="form-control"
-														/>
-														<label className="form-label" for="form3Example3c">
+														<label
+															htmlFor="email"
+															className="form-label"
+															for="form3Example3c"
+														>
 															Your Email
 														</label>
+														<input
+															type="name"
+															id="form3Example3c"
+															className="form-control"
+															name="email"
+															value={values.email}
+															onChange={handleChange}
+															onBlur={handleBlur}
+														/>
 													</div>
 												</div>
 
 												<div className="d-flex flex-row align-items-center mb-4">
 													<i className="fas fa-lock fa-lg me-3 fa-fw"></i>
 													<div className="form-outline flex-fill mb-0">
+														<label
+															htmlFor="password"
+															className="form-label"
+															for="form3Example4c"
+														>
+															Password
+														</label>
 														<input
 															type="password"
 															id="form3Example4c"
 															className="form-control"
+															name="password"
+															value={values.password}
+															onChange={handleChange}
+															onBlur={handleBlur}
 														/>
-														<label className="form-label" for="form3Example4c">
-															Password
-														</label>
 													</div>
 												</div>
 
 												<div className="d-flex flex-row align-items-center mb-4">
 													<i className="fas fa-key fa-lg me-3 fa-fw"></i>
 													<div className="form-outline flex-fill mb-0">
+														<label
+															htmlFor="retype_password"
+															className="form-label"
+															for="form3Example4cd"
+														>
+															Repeat your password
+														</label>
 														<input
 															type="password"
 															id="form3Example4cd"
 															className="form-control"
+															name="retype_password"
+															value={values.retype_password}
+															onChange={handleChange}
+															onBlur={handleBlur}
 														/>
-														<label className="form-label" for="form3Example4cd">
-															Repeat your password
-														</label>
 													</div>
 												</div>
 
-												<div className="form-check d-flex justify-content-center mb-5">
-													<input
-														className="form-check-input me-2"
-														type="checkbox"
-														value=""
-														id="form2Example3c"
-													/>
-													<label
-														className="form-check-label"
-														for="form2Example3"
-													>
-														I agree all statements in{' '}
-														<a href="#!">Terms of service</a>
-													</label>
-												</div>
-
 												<div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-													<button
-														type="button"
+													<input
+														type="submit"
+														value="Register"
 														className="btn btn-primary btn-lg"
-													>
-														Register
-													</button>
+													/>
 												</div>
 											</form>
 										</div>
